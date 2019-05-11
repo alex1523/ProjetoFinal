8c2850031ee7d8730d070378de53f2eebd83ff20
unknown <alexgomes.live@gmail.com>
2019-05-10 22:42:40 -0300

    Pacote 2

--- BD ProjetoFinal.sql
+++ BD ProjetoFinal.sql
@@ -1,9 +1,12 @@
+drop database projetoetec
 create database projetoetec
 use projetoetec
+
+drop table cadastro
 create table cadastro
 (id_codigo  int primary key not null,
  nome  	    varchar (100)   not null,
- cep  	    char (9) 	   not null,
+ cep  	    varchar (8) 	   not null,
  endereco   varchar (100)   not null,
  bairro     varchar (50)    not null,
  cidade     varchar (50)    not null,
---
+++ src/main/java/br/com/etechoracio/projetofinal/model/Registros.java
@@ -0,0 +1,50 @@
+package br.com.etechoracio.projetofinal.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name="cadastro")
+public class Registros {
+	
+	@Id
+	@Column(name="id_codigo")
+	private int id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="cep")
+	private String cep;
+	
+	@Column(name="endereco")
+	private String endereco;
+	
+	@Column(name="bairro")
+	private String bairro;
+	
+	@Column(name="cidade")
+	private String cidade;
+	
+	@Column(name="latitude")
+	private double latitude;
+	
+	@Column(name="longitude")
+	private double longitude;
+	
+	@Column(name="dt_criacao")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCriacao;
+}
